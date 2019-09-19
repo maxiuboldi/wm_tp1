1076401c42e69e5417c28cb9e6c67422623ddaf4
maxiuboldi <maxiuboldi@gmail.com>
2019-09-19 07:53:09 -0300

    v1.0

--- la_nacion/la_nacion_classifiers.py
+++ la_nacion/la_nacion_classifiers.py
@@ -10,16 +10,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-from sklearn.externals import joblib
+import joblib
 
 SEED = 15
 K_FOLD = 5
 JOBS = 4
+N_ITER = 1000
 
 print('Leyendo dataset')
 # Leer el dataset
@@ -38,35 +39,42 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 print('\nGenerando Pipeline')
 pipe = Pipeline([('scl', SelectKBest(score_func=chi2)),
-                 ('clf', RandomForestClassifier(random_state=SEED))])
+                 ('clf', SVC(random_state=SEED))])
 
+# esta forma de definir el espacio de búsqueda con una lista en RandomizedSearchCV está disponible a partir de la versión Version 0.22.dev0.
+# pip install --pre -f https://sklearn-nightly.scdn8.secure.raxcdn.com scikit-learn
 search_space = [{'clf': [LogisticRegression(random_state=SEED)],
-                 'scl__k': [100, 150, 200],
+                 'scl__k': [50, 100, 150, 200, 250],
                  'clf__penalty': ['l1', 'l2'],
-                 'clf__C': np.logspace(-5, 5, 20),
+                 'clf__C': np.logspace(-4, 4, 50),
                  'clf__solver': ['liblinear'],
+                 'clf__max_iter': range(100, 500, 50),
                  'clf__multi_class': ['auto']},
                 {'clf': [RandomForestClassifier(random_state=SEED)],
-                 'scl__k': [100, 150, 200],
+                 'scl__k': [50, 100, 150, 200, 250],
                  'clf__criterion': ['gini', 'entropy'],
                  'clf__min_samples_leaf': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                  'clf__max_depth': range(3, 30, 2),
                  'clf__min_samples_split': [2, 5, 10],
                  'clf__n_estimators': [4, 8, 16, 32, 64, 128, 256, 512]},
                 {'clf': [SVC(random_state=SEED)],
-                 'scl__k': [100, 150, 200],
-                 'clf__kernel': ['linear'],
-                 'clf__C': np.logspace(-5, 5, 20)}]
+                 'scl__k': [50, 100, 150, 200, 250],
+                 'clf__kernel': ['linear', 'rbf', 'poly'],
+                 'clf__degree': [0, 1, 2, 3, 4, 5, 6],
+                 'clf__gamma': ['scale', 'auto'],
+                 'clf__decision_function_shape': ['ovo', 'ovr'],
+                 'clf__C': np.linspace(0.001, 200, 50)}]
 
-gs = GridSearchCV(estimator=pipe,
-                  param_grid=search_space,
-                  scoring='accuracy',
-                  cv=K_FOLD,
-                  n_jobs=JOBS,
-                  verbose=1,
-                  return_train_score=True)
+gs = RandomizedSearchCV(estimator=pipe,
+                        param_distributions=search_space,
+                        scoring='accuracy',
+                        cv=K_FOLD,
+                        n_jobs=JOBS,
+                        verbose=1,
+                        return_train_score=True,
+                        n_iter=N_ITER)
 
-print('\nAjustando Pipeline en GridSearchCV\n')
+print('\nAjustando Pipeline en la búsqueda con CV\n')
 gs.fit(X_train, y_train)
 
 # resultados del CV
